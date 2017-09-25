d04293af5570f995e4d6936e1e647d938eb571e8
joshua9889 <heinrich.joshua@student.ccm.edu>
2017-09-25 13:27:07 -0400

    Classwork from 9/25/17

---
+++ 9.25.17/Lab3.py
@@ -0,0 +1,20 @@
+# Return the Fahrenheit value of a celsius value
+def celsius2Fahrenheit(celsius):
+    degreeF = degreeC*9./5. +32 # Calculate Fahrenheit.
+    return degreeF
+
+# 2 Function that computes the factorial for a number.
+# 2.1 For numbers that are zero or less, the function returns 0.
+# 2.2 For number equal to one, it returns 1.
+# 2.3 For any positive integer greater than one, it computes the product term of
+#       the 'n' plus all of the integers between the number and one.
+def factorial(number):
+    if number <= 0: # 2.1
+        return 0
+    elif number == 1: # 2.2
+        return number
+    elif number > 1: # 2.3
+       result = 1;
+       for i in range(1,number+1):
+           result *= i;
+       return result;
---
+++ 9.25.17/function1.py
@@ -0,0 +1,29 @@
+# Code from
+def say_hello():
+    # block belonging to the function
+    print('hello world')
+
+# End of function
+say_hello() # call the function
+say_hello() # call the function again
+
+def print_max(a, b):
+     if a > b:
+         print(a, 'is maximum')
+     elif a == b:
+         print(a, 'is equal to', b)
+     else:
+         print(b, 'is maximum')
+
+def maximum(x, y):
+     if x > y:
+         return x
+     elif x == y:
+         return 'The numbers are equal'
+     else:
+         return y
+
+def func(x):
+     print 'x is', x
+     x = 2
+     print 'Changed local x to', x
